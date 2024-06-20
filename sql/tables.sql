1b62a330fcc8abef6b5e13f7cc5b8884f3bc0191
Elliot <141949359+webElliot@users.noreply.github.com>
2024-06-20 18:21:34 +0100

    added old_players table to reduce load on scraper and focus on new players only

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,28 +1,37 @@
-
-
 -- The players table storing player_id's, Name's and first appearances to the database
-CREATE TABLE IF NOT EXISTS players(
-    ID SERIAL PRIMARY KEY,
-    NAME CITEXT UNIQUE NOT NULL,
-    FIRST_SEEN timestamptz NOT NULL DEFAULT NOW()
+CREATE TABLE IF NOT EXISTS players
+(
+    ID         SERIAL PRIMARY KEY,
+    NAME       CITEXT UNIQUE NOT NULL,
+    FIRST_SEEN timestamptz   NOT NULL DEFAULT NOW()
 );
 
+-- These players do not need to be scraped they are low priority.
+CREATE TABLE IF NOT EXISTS old_players
+(
+    ID         SERIAL,
+    NAME       CITEXT PRIMARY KEY NOT NULL ,
+    FIRST_SEEN timestamptz   NOT NULL DEFAULT NOW()
+);
 
 -- The user is not found on Hiscores
 -- This can mean: Banned || Username was changed
-CREATE TABLE IF NOT EXISTS not_found(
-   PlayerId SERIAL PRIMARY KEY,
-   FIRST_SEEN timestamptz NOT NULL DEFAULT NOW(),
-   FOREIGN KEY (PlayerId) REFERENCES players(id)
+CREATE TABLE IF NOT EXISTS not_found
+(
+    PlayerId   SERIAL PRIMARY KEY,
+    FIRST_SEEN timestamptz NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (PlayerId) REFERENCES players (id)
 );
 
 -- Low Latency Live player stats
-CREATE TABLE IF NOT EXISTS player_live(
-    PlayerId INT PRIMARY KEY,
-    LAST_UPDATED timestamptz not null default NOW(),
+CREATE TABLE IF NOT EXISTS player_live
+(
+    PlayerId          INT PRIMARY KEY,
+    LAST_UPDATED      timestamptz not null default NOW(),
     skills_experience JSONB,
-    skills_levels JSONB,
-    skills_ratio JSONB,
-    minigames JSONB,
-    FOREIGN KEY (PlayerId) REFERENCES players(id)
+    skills_levels     JSONB,
+    skills_ratio      JSONB,
+    minigames         JSONB,
+    FOREIGN KEY (PlayerId) REFERENCES players (id)
 );
+
