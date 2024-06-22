4031a5d136302054ff3cf40e45df93c0e05a1a4a
Elliot <141949359+webElliot@users.noreply.github.com>
2024-06-22 17:34:11 +0100

    added combat calculator for simplePlayer

---
+++ src/internal/utils/stats/combatLevel.go
@@ -0,0 +1,41 @@
+package stats
+
+import (
+	"math"
+)
+
+func (sp *SimplePlayer) calculateCombat() int16 {
+	combatLvl := CombatLevel(sp.SkillLevels["Attack"], sp.SkillLevels["Defence"], sp.SkillLevels["Strength"], sp.SkillLevels["Hitpoints"], sp.SkillLevels["Prayer"], sp.SkillLevels["Ranged"], sp.SkillLevels["Magic"])
+	return combatLvl
+}
+
+func minimumCombatLevel(combatLevel int16) int16 {
+	var minimumLevel int16 = 3
+	if combatLevel < minimumLevel {
+		return minimumLevel
+	}
+	return combatLevel
+}
+
+func CombatLevel(attack, defence, strength, hitpoints, prayer, ranged, magic int) int16 {
+	attack = int(removeVirtualLevel(int16(attack)))
+	defence = int(removeVirtualLevel(int16(defence)))
+	strength = int(removeVirtualLevel(int16(strength)))
+	hitpoints = int(removeVirtualLevel(int16(hitpoints)))
+	prayer = int(removeVirtualLevel(int16(prayer)))
+	ranged = int(removeVirtualLevel(int16(ranged)))
+	magic = int(removeVirtualLevel(int16(magic)))
+
+	base := float64(defence+hitpoints+prayer/2) * 0.25
+
+	melee := float64(attack+strength) * 0.325
+	rng := float64(math.Floor(float64(ranged)*1.5)) * 0.325
+	mage := float64(math.Floor(float64(magic)*1.5)) * 0.325
+	maximumLevel := math.Max(melee, math.Max(rng, mage))
+	level := math.Round((base+maximumLevel)*1000) / 1000
+	return minimumCombatLevel(int16(math.Floor(level)))
+}
+
+func example() {
+	_ = CombatLevel(42, 40, 40, 41, 44, 35, 41)
+}
